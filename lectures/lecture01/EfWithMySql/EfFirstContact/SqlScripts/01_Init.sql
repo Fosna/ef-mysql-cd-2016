b221c03f5b1ae1b04c9419a5bff403659aca161c
Fosna <vedran.marsic.ka@gmail.com>
2016-06-11 14:55:25 +0200

    Rest of initial sql script added.

--- lectures/lecture01/EfWithMySql/EfFirstContact/SqlScripts/01_Init.sql
+++ lectures/lecture01/EfWithMySql/EfFirstContact/SqlScripts/01_Init.sql
@@ -1,13 +1,19 @@
-﻿drop database if exists todo;
+﻿drop database if exists Todo;
 
-create database todo;
+create database Todo;
+use Todo;
 
-use todo;
+drop table if exists TodoItem;
 
-create table todoItem (
-	id int auto_increment primary key,
-    description nvarchar(1024) not null
+create table TodoItem (
+	Id int auto_increment primary key,
+    Description nvarchar(1024) not null,
+    TimeCreated timestamp not null,
+    TimeSetToDone timestamp null
 );
 
-select *
-from todoItem;
+insert into TodoItem(Description, TimeCreated, TimeSetToDone) values 
+('Buy milk', current_timestamp() - interval 1 hour, null),
+('Call Miki', current_timestamp() - interval 1 hour, null),
+('Do homework', current_timestamp() - interval 4 hour, current_timestamp() - interval 2 hour);
+
