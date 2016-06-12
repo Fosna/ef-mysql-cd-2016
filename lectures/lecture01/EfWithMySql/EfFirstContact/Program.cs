f2f20c62c5cb9ae605288a72b51ba0d31b5cf778
Fosna <vedran.marsic.ka@gmail.com>
2016-06-12 14:42:44 +0200

    Comments added.

--- lectures/lecture01/EfWithMySql/EfFirstContact/Program.cs
+++ lectures/lecture01/EfWithMySql/EfFirstContact/Program.cs
@@ -10,8 +10,12 @@ namespace EfFirstContact
     {
         static void Main(string[] args)
         {
+            // Create database context. Context is generated from ADO.NET Entity Data Model database first wizard. 
             var db = new todoEntities();
+            // db.todoitem is table representation of TodoItem table in database.
+            // Any is extension method that checks if there are any rows in database.
             var todoItemsExist = db.todoitem.Any();
+
             Console.WriteLine(todoItemsExist);
         }
     }
