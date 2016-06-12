dda275432636664fff213ad76aba6057dfc27fec
Fosna <vedran.marsic.ka@gmail.com>
2016-06-12 15:23:39 +0200

    List all todo items to console refactored to TodoApp class.

--- lectures/lecture01/EfWithMySql/EfFirstContact/Program.cs
+++ lectures/lecture01/EfWithMySql/EfFirstContact/Program.cs
@@ -10,25 +10,8 @@ namespace EfFirstContact
     {
         static void Main(string[] args)
         {
-            // Create database context. Context is generated from ADO.NET Entity Data Model database first wizard. 
-            var db = new todoEntities();
-            // db.todoitem is table representation of TodoItem table in database.
-            List<todoitem> todoItemList = db.todoitem.ToList();
-
-            // Write each todo item to console.
-            foreach (var todoItem in todoItemList)
-            {
-                Console.WriteLine($"Id: {todoItem.Id}");
-                Console.WriteLine($"Description: {todoItem.Description}");
-                Console.WriteLine($"Created: {todoItem.TimeCreated}");
-                var isDone = false;
-                if (todoItem.TimeSetToDone.HasValue)
-                {
-                    isDone = true;
-                }
-                Console.WriteLine($"Done?: {isDone}");
-                Console.WriteLine();
-            }
+            var app = new TodoApp();
+            app.List();
         }
     }
 }
---
+++ lectures/lecture01/EfWithMySql/EfFirstContact/TodoApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace EfFirstContact
+{
+    internal class TodoApp
+    {
+        private todoEntities db;
+
+        public TodoApp()
+        {
+            this.db = new todoEntities();
+        }
+
+        public void List()
+        {
+            var todoItemList = db.todoitem.ToList();
+
+            foreach (var todoItem in todoItemList)
+            {
+                Console.WriteLine($"Id: {todoItem.Id}");
+                Console.WriteLine($"Description: {todoItem.Description}");
+                Console.WriteLine($"Created: {todoItem.TimeCreated}");
+                var isDone = false;
+                if (todoItem.TimeSetToDone.HasValue)
+                {
+                    isDone = true;
+                }
+                Console.WriteLine($"Done?: {isDone}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
