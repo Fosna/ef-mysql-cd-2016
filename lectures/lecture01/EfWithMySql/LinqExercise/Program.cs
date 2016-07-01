df0c09b765e628182ceb4c06854416dbe05b09e0
Fosna <vedran.marsic.ka@gmail.com>
2016-07-01 21:07:14 +0200

    UseLoopToFindElements. Linq exercise.

--- lectures/lecture01/EfWithMySql/LinqExercise/Program.cs
+++ lectures/lecture01/EfWithMySql/LinqExercise/Program.cs
@@ -9,15 +9,51 @@ namespace LinqExercise
 {
     class Program
     {
-        private static object teenagerStudent;
-
         static void Main(string[] args)
         {
+            UseLoopToFindElements();
             LinqQuerySyntax();
+        }
+
+        private static void UseLoopToFindElements()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("******* UseLoopToFindElements");
+
+            Student[] studentArray = 
+            {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+                new Student() { StudentID = 2, StudentName = "Steve", Age = 21 },
+                new Student() { StudentID = 3, StudentName = "Bill", Age = 25 },
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+                new Student() { StudentID = 6, StudentName = "Chris", Age = 17 },
+                new Student() { StudentID = 7, StudentName = "Rob", Age = 19  },
+            };
+
+            Student[] students = new Student[10];
+
+            var filteredStudents = new List<Student>();
+            foreach (Student std in studentArray)
+            {
+                if (std.Age > 12 && std.Age < 20)
+                {
+                    filteredStudents.Add(std);
+                }
+            }
+
+            Console.WriteLine(filteredStudents.DumpJson());
         }
 
         private static void LinqQuerySyntax()
         {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("******* LinqQuerySyntax");
+
             // Student collection
             IList<Student> studentList = new List<Student>()
             {
@@ -29,7 +65,7 @@ namespace LinqExercise
             };
 
             // LINQ Query Syntax to find out teenager students
-            var teenagerStudent = 
+            var teenagerStudent =
                 from s in studentList
                 where s.Age > 12 && s.Age < 20
                 select s;
